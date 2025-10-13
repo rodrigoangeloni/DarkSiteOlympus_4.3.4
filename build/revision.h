c82dc69d3593948e70fd869f959b4a49edb3a7c7
rodrigoangeloni <rodrigo_angeloni@hotmail.com>
2025-10-12 21:10:21 -0300

    Actualización: soporte para CMake 3.24+ y OpenSSL 3.x, mejoras de compatibilidad y modernización del proyecto

---
+++ build/revision.h
@@ -0,0 +1,11 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define _HASH                      "dd38c825f6fc+"
+ #define _DATE                      "2025-09-29 22:45:47 -0600"
+ #define VER_COMPANYNAME_STR        "OlympusCore Developers"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2023 OlympusCore"
+ #define VER_FILEVERSION            0,0,0
+ #define VER_FILEVERSION_STR        "2025-09-29 22:45:47 -0600 (main)"
+ #define VER_PRODUCTVERSION         VER_FILEVERSION
+ #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
+#endif // __REVISION_H__
